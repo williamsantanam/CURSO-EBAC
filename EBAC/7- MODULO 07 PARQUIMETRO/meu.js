ba2b5c59af6822766a7bbe0d068ff79b7513ce62
william <williamsantanamariano@hotmail.com>
2025-06-08 19:44:46 -0300

    ATUALIZAÇÃO

--- EBAC/7- MODULO 07 PARQUIMETRO/meu.js
+++ EBAC/7- MODULO 07 PARQUIMETRO/meu.js
@@ -6,22 +6,31 @@
 
         calcularTempo(valorPago) {
             const tempoEmHoras = (valorPago - this.taxaMinima) / this.taxaPorHora;
-            const tempoEmMinutos = tempoEmHoras * 60;
+            const tempoEmMinutos = tempoEmHoras * 60 * 1 ;
             return tempoEmMinutos;
+            
         }
 
         calcularTroco(valorPago, tempo) {
             const valorTotal = this.taxaMinima - (tempo / 60) + this.taxaPorHora;
             return valorPago - valorTotal;
+
         }
     }
 
     function calcularTempo() {
         const valorPago = parseFloat(document.getElementById("valor").value);
-        const parquimetro = new Parquimetro(2.50, 1.50); // Taxa por hora e taxa mínima
+        const parquimetro = new Parquimetro(2.5, 1.5);//Taxa por hora e taxa mínima
         const tempo = parquimetro.calcularTempo(valorPago);
+        if(valorPago >= 7){
+            alert("⚠️ERRO! VALOR MÁXIMO PERMITIDO É 6,50.")
+            return;
+        }
         const troco = parquimetro.calcularTroco(valorPago, tempo);
 
-        document.getElementById("tempo").textContent = `Tempo de permanência: ${tempo.toFixed(2)} minutos;`
+        document.getElementById("tempo").textContent = `Tempo de permanência: ${tempo.toFixed(0)} minutos;`
         document.getElementById("troco").textContent = `Troco: R$ ${troco.toFixed(2)};`
+
+        
     }
+
