19cfd8e0922ebd9010f544a5850e749076b82e7b
William Santana Mariano <williamsantanamariano@hotmail.com>
2025-07-02 12:30:15 -0300

    Update meu.js

--- EBAC/7- MODULO 07 PARQUIMETRO/meu.js
+++ EBAC/7- MODULO 07 PARQUIMETRO/meu.js
@@ -1,27 +1,55 @@
 class Parquimetro {
-      constructor() {
-        // Tabela de preços com base no tempo em minutos
-        this.tabelaPrecos = {
-          30: 1.00,
-          60: 1.75,
-          120: 3.00
-        }
-      }
+  constructor(valorPago) {
+    this.valorPago = valorPago;
+    this.tempo = 0;
+    this.troco = 0;
+    this.valorInsuficiente = false;
+    this.pare
+  }
 
-      calcularPreco(tempo) {
-        if (this.tabelaPrecos[tempo]) {
-          return this.tabelaPrecos[tempo];
-        } else {
-          return "Tempo inválido.";
-        }
-      }
+  calcular() {
+
+    if (this.valorPago >= 1 && this.valorPago < 1.75) {
+      this.tempo = 30;
+      this.troco = this.subtrai(this.valorPago, 1);
+    } 
+    else if (this.valorPago >= 1.75 && this.valorPago < 3.00) {
+      this.tempo = 60;
+      this.troco = this.subtrai(this.valorPago, 1.75);
+    } 
+    else if (this.valorPago >= 3) {
+      this.tempo = 120;
+      this.troco = this.subtrai(this.valorPago, 3);
+    } 
+    else {
+      this.valorInsuficiente = true;
+    }
+    if (this.valorPago >= 3.01) {
+      document.getElementById('resultado'). textContent = "Valor permitido R$: 1, 1.75 ou 3!";
+      return;
     }
 
-    // Instância do parquímetro
-    const parquimetro = new Parquimetro();
-
-    function calcular() {
-      const tempo = parseInt(document.getElementById("tempo").value);
-      const preco = parquimetro.calcularPreco(tempo);
-      document.getElementById("resultado").innerText = typeof preco === "number"? `Valor a pagar: R$ ${preco.toFixed(2)}`: preco;
+    if (this.valorInsuficiente) {
+      document.getElementById('resultado').textContent = "Valor insuficiente";
+    } 
+    else {
+      document.getElementById('resultado').textContent = `O tempo de permanência foi ${this.tempo} minutos e o troco foi R$ ${this.troco}`;
     }
+
+  }
+
+  subtrai(valorPago, valorTempo) {
+    return valorPago - valorTempo;
+  }
+
+}
+
+function calcular() {
+  let valorInserido = document.getElementById('valor').value;
+  const parquimetroNovo = new Parquimetro(valorInserido);
+  parquimetroNovo.calcular();
+}
+
+function limpar() {
+  document.getElementById('valor').value = '';
+}
